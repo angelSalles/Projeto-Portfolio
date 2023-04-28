44a573d57eb3eda02c4fd0aff92bcab31fdafcb6
angelSalles <angelgabrieldesouzasales@gmail.com>
2023-04-28 09:40:51 -0400

    <STYLE>[Home]: Responsabilidade do carregamento do background-image para o js v1.1.0

---
+++ js/background.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function() {
+    let fundo = document.getElementById('corpo');
+    fundo.style.backgroundImage = "url(/assets/background.png)";
+    fundo.style.backgroundRepeat = "no-repeat";
+    fundo.style.backgroundSize = "cover";
+});
